71f31f42632c9ccae183fa334129591e5eb23c3f
Paul Kitching <34681229+Cernus@users.noreply.github.com>
2023-10-22 23:38:34 +0100

    Initial commit

---
+++ Railpen.Data/ViewModels/QuoteVM.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Railpen.Data.ViewModels
+{
+    public class QuoteVM
+    {
+        public string Name { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+
+        [DisplayName("Phone Number")]
+        [Phone]
+        public string PhoneNumber { get; set; }
+
+        public string Company { get; set; }
+
+        public QuoteVM()
+        {
+            Name = string.Empty;
+            Email = string.Empty;
+            PhoneNumber = string.Empty;
+            Company = string.Empty;
+        }
+    }
+}
---
+++ Railpen.Web/wwwroot/js/multistepform.js
@@ -0,0 +1,95 @@
+﻿/**
+ * Define a function to navigate betweens form steps.
+ * It accepts one parameter. That is - step number.
+ */
+const navigateToFormStep = (stepNumber) => {
+    /**
+     * Validation
+     */
+    const name = document.getElementById('name');
+    const email = document.getElementById('email');
+    const errorElement = document.getElementById('error');
+
+    let messages = [];
+    if (name.value == '' || name.value == null) {
+        messages.push('Name is Required');
+    }
+    if (email.value == '' || email.value == null) {
+        messages.push('Email address is Required');
+    }
+
+    if (messages.length > 0) {
+        errorElement.innerText = messages.join(', ');
+        return;
+    }
+    errorElement.innerText = '';
+
+    /**
+     * Hide all form steps.
+     */
+    document.querySelectorAll(".form-step").forEach((formStepElement) => {
+        formStepElement.classList.add("d-none");
+    });
+    /**
+     * Mark all form steps as unfinished.
+     */
+    document.querySelectorAll(".form-stepper-list").forEach((formStepHeader) => {
+        formStepHeader.classList.add("form-stepper-unfinished");
+        formStepHeader.classList.remove("form-stepper-active", "form-stepper-completed");
+    });
+    /**
+     * Show the current form step (as passed to the function).
+     */
+    document.querySelector("#step-" + stepNumber).classList.remove("d-none");
+    document.querySelector("#step-title-" + stepNumber).classList.remove("d-none");
+    /**
+     * Select the form step circle (progress bar).
+     */
+    const formStepCircle = document.querySelector('li[step="' + stepNumber + '"]');
+    /**
+     * Mark the current form step as active.
+     */
+    formStepCircle.classList.remove("form-stepper-unfinished", "form-stepper-completed");
+    formStepCircle.classList.add("form-stepper-active");
+    /**
+     * Loop through each form step circles.
+     * This loop will continue up to the current step number.
+     * Example: If the current step is 3,
+     * then the loop will perform operations for step 1 and 2.
+     */
+    for (let index = 0; index < stepNumber; index++) {
+        /**
+         * Select the form step circle (progress bar).
+         */
+        const formStepCircle = document.querySelector('li[step="' + index + '"]');
+        /**
+         * Check if the element exist. If yes, then proceed.
+         */
+        if (formStepCircle) {
+            /**
+             * Mark the form step as completed.
+             */
+            formStepCircle.classList.remove("form-stepper-unfinished", "form-stepper-active");
+            formStepCircle.classList.add("form-stepper-completed");
+        }
+    }
+};
+/**
+ * Select all form navigation buttons, and loop through them.
+ */
+document.querySelectorAll(".btn-navigate-form-step").forEach((formNavigationBtn) => {
+
+    /**
+     * Add a click event listener to the button.
+     */
+    formNavigationBtn.addEventListener("click", () => {
+        /**
+         * Get the value of the step.
+         */
+        const stepNumber = parseInt(formNavigationBtn.getAttribute("step_number"));
+        /**
+         * Call the function to navigate to the target form step.
+         */
+        navigateToFormStep(stepNumber);
+    });
+});
